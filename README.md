80ff01ba7d690be9c5d30fd774e1156fb94b1d69
Pedro Hitoshi <35139632+pedrohitoshi@users.noreply.github.com>
2023-11-15 17:38:55 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para estudos do tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-  return <h1>Amorzinho, eu te amo! Se você me ama, dá um sorrisinho 🥰</h1>
+function Home() {
+  return <h1>Amorzinho, eu te amo! Se você me ama, dá um sorrisinho 🥰</h1>;
 }
 
 export default Home;
